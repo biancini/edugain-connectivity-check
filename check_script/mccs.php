a8f34773c0d8ab39c35e3d68e33e47ecf8a3e9d2
Andrea Biancini <andrea.biancini@gmail.com>
2015-02-16 11:53:00 +0100

    Script inserts data into DB

--- check_script/mccs.php
+++ check_script/mccs.php
@@ -4,11 +4,25 @@ include ("utils.php");
             
 $conf_array = parse_ini_file(dirname(__FILE__) . '/../properties.ini', true);
 $map_url = $conf_array['check_script']['map_url'];
-$spEntityID_1 = $conf_array['check_script']['spEntityID_1'];
-$spEntityID_2 = $conf_array['check_script']['spEntityID_2'];
-$spACSurl_1 = $conf_array['check_script']['spACSurl_1'];
-$spACSurl_2 = $conf_array['check_script']['spACSurl_2'];
-            
+
+$spEntityIDs = array();
+$spEntityIDs[] = $conf_array['check_script']['spEntityID_1'];
+$spEntityIDs[] = $conf_array['check_script']['spEntityID_2'];
+
+$spACSurls = array();
+$spACSurls[] = $conf_array['check_script']['spACSurl_1'];
+$spACSurls[] = $conf_array['check_script']['spACSurl_2'];
+
+if (count($spEntityIDs) != count($spACSurls)) {
+	die("Configuration error. Please check properties.ini.");
+}
+
+$db_host = $conf_array['db_connection']['db_host'];
+$db_port = $conf_array['db_connection']['db_port'];
+$db_name = $conf_array['db_connection']['db_name'];
+$db_user = $conf_array['db_connection']['db_user'];
+$db_password = $conf_array['db_connection']['db_password'];
+
 $arrContextOptions=array(
 	"ssl"=>array(
 		"verify_peer"=>false,
@@ -27,34 +41,91 @@ if (($metadataXML = file_get_contents($map_url, false, stream_context_create($ar
 			echo "\t", $error->message;
 		}
 	} else {
+		$mysqli = new mysqli($db_host, $db_user, $db_password, $db_name, $db_port);
+		if ($mysqli->connect_errno) {
+			die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+		}
+
 		$count = 0;
 		foreach ($idpList as $idp){
 			$count++;
-					
-			$result_1 = checkIdp($idp['SingleSignOnService'], $spEntityID_1, $spACSurl_1);
-			$result_2 = checkIdp($idp['SingleSignOnService'], $spEntityID_2, $spACSurl_2);
-			
-			$keyToCheck = array("ok", "form_valid", "http_code", "curl_return", "messages");
-			foreach ($keyToCheck as $key){
-				if( $result_1[$key] !== $result_2[$key] ){
-					echo "The IdP ".$idp['entityID']." NOT consumpt metadata correctly";
-					echo "<br><br>";
+
+			$previous_status = NULL;
+			$check_ok = true;
+			$reason = 'OK';
+			$messages = array();
+
+			$sql = "SELECT * FROM EntityDescriptors WHERE entityID = '" . $idp['entityID'] . "' ORDER BY lastCheck";
+			$result = $mysqli->query($sql) or die("Error: " . $sql . ": " . mysqli_error($mysqli));
+
+			if ($result->num_rows > 0) {
+				while ($row = $result->fetch_assoc()) {
+					$previous_status = $row['currentResult'];
 				}
+			} else {
+				$sql  = 'INSERT INTO EntityDescriptors (entityID, registrationAuthority, technicalContacts, supportContacts) VALUES (';
+				$sql .= "'" . $idp['entityID'] . "', ";
+				$sql .= "'" . $idp['registrationAuthority'] . "', ";
+				$sql .= "'" . join(",", $idp['technicalContacts']) . "', ";
+				$sql .= "'" . join(",", $idp['supportContacts']) . "'";
+				$sql .= ")";
+
+				$mysqli->query($sql) or die("Error: " . $sql . ": " . mysqli_error($mysqli));
 			}
 
-// 			foreach ($result_1 as $key => $value) {
-// 				if ($key == "messages"){
-// 					$cnt = 0;
-// 					foreach ($value as $v){
-// 						$cnt++;
-// 						echo "Messages $cnt: $v\n";
-// 					}
-// 				} else {
-// 					echo "$key => $value\n";
-// 				}
-// 			}
-// 			echo "\n";
+			for ($i = 0; $i < count($spEntityIDs); $i++) {
+				$result = checkIdp($idp['SingleSignOnService'], $spEntityIDs[$i], $spACSurls[$i]);
+
+				// fai insert in tabella EntityChecks
+				$sql  = 'INSERT INTO EntityChecks (entityID, spEntityID, checkHtml, httpStatusCode, checkResult) VALUES (';
+				$sql .= "'" . $idp['entityID'] . "', ";
+				$sql .= "'" . $spEntityIDs[$i] . "', ";
+				$sql .= "'" . mysql_real_escape_string($result['html']) . "', ";
+				$sql .= $result['http_code'] . ", ";
+
+				if ($result['ok']) {
+					$sql .= "'OK'";
+				} else {
+					$check_ok = false;
+					$messages = $result['messages'];
+
+					if (!$result['form_valid']) {
+						$reason = 'FORM-Invalid';
+					}
+					elseif ($result['http_code'] != 200) {
+						$reason = 'HTTP-Error';
+					}
+					elseif ($result['curl_return'] != '') {
+						$reason = 'CURL-Error';
+					}
+
+					$sql .= "'" . $reason . "'";
+				}
+
+				$sql .= ")";
+				$mysqli->query($sql) or die("Error: " . $sql . ": " . mysqli_error($mysqli));
+
+				// update EntityDescriptors
+				$sql = "UPDATE EntityDescriptors SET ";
+				$sql .= "currentResult = '" . $reason . "' ";
+				if ($previous_status != NULL) $sql .= ", previousResult = '" . $previous_status . "' ";
+				$sql .= "WHERE entityID = '" . $idp['entityID'] . "' ";
+				$mysqli->query($sql) or die("Error: " . $sql . ": " . mysqli_error($mysqli));
+			}
+
+			if ($check_ok) {
+				//echo "The IdP ".$idp['entityID']." consumed metadata correctly\n";
+			}
+			else {
+				echo "The IdP ".$idp['entityID']." did NOT consume metadata correctly.\n";
+				echo "Reason: " . $reason . "\n";
+				echo "Messages: " . print_r($messages, true) . "\n";
+			}
+			echo "\n";
 		}
+
+		$mysqli->close();
 	}
 }
+
 ?>
