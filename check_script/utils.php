200e194c2b02f542a1443b6fe7e7c55f57ae3d98
Andrea Biancini <andrea.biancini@gmail.com>
2015-03-11 21:02:14 +0100

    Fixed import

--- check_script/utils.php
+++ check_script/utils.php
@@ -11,7 +11,7 @@
 # limitations under the License.
 
 
-include ("../PHPMailer/PHPMailerAutoload.php");
+include (dirname(__FILE__)."/../PHPMailer/PHPMailerAutoload.php");
 
 /**
  Create a new DB connection and return its pointer.
