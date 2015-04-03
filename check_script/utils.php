f9859decf51381cb2fa6aa07c67d300320d034cc
Andrea Biancini <andrea.biancini@gmail.com>
2015-04-03 15:09:52 +0200

    Faking browser headers

--- check_script/utils.php
+++ check_script/utils.php
@@ -441,7 +441,16 @@ function checkIdp($httpRedirectServiceLocation, $spEntityID, $spACSurl) {
           CURLOPT_RETURNTRANSFER => true,
           CURLOPT_TIMEOUT => 45,
           CURLOPT_CONNECTTIMEOUT => 20,
-          CURLOPT_SSLVERSION => $vers
+          CURLOPT_SSLVERSION => $vers,
+          CURLOPT_HTTPHEADER => array(
+                'User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12',
+                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language: en-us,en;q=0.5',
+                'Accept-Encoding: gzip,deflate',
+                'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7',
+                'Keep-Alive: 115',
+                'Connection: keep-alive',
+          )
        ));
        $html = curl_exec($curl);
        if ($html === false) {
