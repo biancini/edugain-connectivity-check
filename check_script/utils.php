af5cb63da80125b94eba6bfe0112cf6e19e448b6
Marco Malavolti <marco.malavolti@gmail.com>
2015-04-07 12:14:47 +0200

    Fixed the problem encountered with some IdPs of Renater Federation.

--- check_script/utils.php
+++ check_script/utils.php
@@ -442,15 +442,7 @@ function checkIdp($httpRedirectServiceLocation, $spEntityID, $spACSurl) {
           CURLOPT_TIMEOUT => 45,
           CURLOPT_CONNECTTIMEOUT => 20,
           CURLOPT_SSLVERSION => $vers,
-          CURLOPT_HTTPHEADER => array(
-                'User-Agent: Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.2.12) Gecko/20101026 Firefox/3.6.12',
-                'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-                'Accept-Language: en-us,en;q=0.5',
-                'Accept-Encoding: gzip,deflate',
-                'Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7',
-                'Keep-Alive: 115',
-                'Connection: keep-alive',
-          )
+          CURLOPT_USERAGENT => 'Mozilla/5.0 (Windows NT 6.3; WOW64; rv:36.0) Gecko/20100101 Firefox/36.0',
        ));
        $html = curl_exec($curl);
        if ($html === false) {
