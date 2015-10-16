b61b94b62f9454904671db2abac099a5d6ad7405
Andrea Biancini <andrea.biancini@garr.it>
2015-10-16 09:02:40 +0000

    Fixed sonar integration for Angular

--- tests/view/karma.config.js
+++ tests/view/karma.config.js
@@ -35,6 +35,8 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+      'web/js/eccs.js': 'coverage',
+      'web/js/eccs-*.js': 'coverage'
     },
 
 
