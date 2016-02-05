32e2f6a6ca051f8a81907d4e50a1fbe796836096
Marco Malavolti <marco.malavolti@garr.it>
2016-02-05 09:07:47 +0100

    Removed PHPphantomJS directory to be able to add again as submodule

--- PHPphantomJS/autoload.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function($class_name){
-	$file_name = str_replace('\\', '/', $class_name);
-	$file_name = str_replace('_', '/', $file_name);
-	$file = dirname(__FILE__) . "/src/$file_name.php";
-	if(is_file($file)) include $file;
-});
